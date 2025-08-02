6dca2217eb07f58b6bee5a087b88235064377524
Robbie <robert.howe@student.uts.edu.au>
2025-08-02 23:06:41 +1000

    clean up

--- Source/Headers/GameManager.h
+++ Source/Headers/GameManager.h
@@ -5,8 +5,6 @@
 #include "CathedralState.h"
 #include "PlayerTurn.h"
 
-// Forward declaration
-class BoardUtility;
 
 class GameManager {
 public:
@@ -37,8 +35,6 @@ private:
     void performMCTSMove();
     bool isValidPlayerPiece(int pieceIndex) const;
     int getCurrentPlayer() const;
-    void testTerritoryDebug();  // Test territory checking with debug visualization
-    void testTerritoryForMove(const Cathedral_move& move);  // Test territory for a specific move
     
     // Core game objects
     sf::RenderWindow& window;
@@ -65,8 +61,4 @@ private:
     int _playerMin;
     int _playerMax;
     
-    // Debug state
-    bool debugTerritoryMode = false;
-    std::unique_ptr<BoardUtility> debugBoardUtil;
-    bool stepByStepFloodFillMode = false;
 };
